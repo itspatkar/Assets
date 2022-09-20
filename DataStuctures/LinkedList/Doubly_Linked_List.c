577949fd2d53d3012bb50f8d741f69b83a10c642
itspatkar <94187044+itspatkar@users.noreply.github.com>
2022-09-20 23:18:23 +0530

    Updated

---
+++ DataStuctures/LinkedList/Doubly_Linked_List.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+	int data;
+	struct node *prev;
+	struct node *next;
+};
+
+struct node *head = NULL;
+struct node *current = NULL;
+
+int int main(int argc, char const *argv[]){
+	
+	return 0;
+}
