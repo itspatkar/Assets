db59f7ee497e8d0948d817f5f12124889c6b887a
itspatkar <94187044+itspatkar@users.noreply.github.com>
2022-09-27 22:30:12 +0530

    Updated

--- Cheatsheets/dryrun.cpp
+++ Cheatsheets/dryrun.cpp
@@ -1,19 +1,8 @@
 #include <iostream>
-#include <cstring>
 using namespace std;
 
 
-struct Classroom {
-    int roll;
-    char name[10];
-    struct Classroom *ptr;
-};
-
 int main(){
-    struct Classroom c1 = { 17, "Mandar", NULL};
-
-    c1.ptr=&c1;
-    cout << "Roll: "<<c1.ptr->roll << "\nName: "<< c1.ptr->name;
 
     return 0;
 }
