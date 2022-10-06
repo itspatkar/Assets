02ca149dfe41fd4fb72b5f72edb2f18986182101
itspatkar <94187044+itspatkar@users.noreply.github.com>
2022-10-06 19:21:27 +0530

    Updated

--- Cheatsheets/dryrun.cpp
+++ Cheatsheets/dryrun.cpp
@@ -1,15 +1,6 @@
 #include <iostream>
 using namespace std;
 
-
 int main(){
-    int v = 22;
-// Referencing:
-cout << "Address of v is " << &v;
-// Dereferencing:
-cout << "\nValue of v is " << *(&v);
-
-
-
     return 0;
 }
