b47117d25196e4efe626078fdc1d09a2987e95f9
itspatkar <94187044+itspatkar@users.noreply.github.com>
2022-10-10 00:49:04 +0530

    Updated

--- DataStuctures/LinkedList/Doubly_Linked_List.c
+++ DataStuctures/LinkedList/Doubly_Linked_List.c
@@ -15,11 +15,11 @@ void printData(struct node *head){
 	if(head == NULL)
 		printf("\nList is empty!");
 	else {
-		struct node *temp = head;
+		struct node *ptr = head;
 		printf("\nData:");
-		while(temp != NULL){
-			printf(" %d",temp->data);
-			temp = temp->next;
+		while(ptr != NULL){
+			printf(" %d",ptr->data);
+			ptr = ptr->next;
 		}
 	}
 }
@@ -45,14 +45,90 @@ struct node* insertAtBeg(struct node *head, int data){
 
 // Insert Node at End of Doubly Linked List -
 struct node* insertAtEnd(struct node *head, int data){
-	current = head;
-	while(current->next != NULL)
-		current = current->next;
 	struct node *temp = malloc(sizeof(struct node));
-	temp->prev = current;
+	temp->prev = NULL;
 	temp->data = data;
 	temp->next = NULL;
-	current->next = temp;
+	struct node *ptr = head;
+	while(ptr->next != NULL)
+		ptr = ptr->next;
+	temp->prev = ptr;
+	ptr->next = temp;
+	return head;
+}
+
+// Inserting Node at Certain Position in Linked List -
+struct node* insertAtPos(struct node *head, int data, int pos){
+	if(pos == 1)
+		head = insertAtBeg(head,data);
+	else {
+		struct node *temp = malloc(sizeof(struct node));
+		temp->prev = NULL;
+		temp->data = data;
+		temp->next = NULL;
+		struct node *ptr = head;
+		struct node *ptr2 = head;
+		int count = 1;
+		while(count != pos-1){
+			ptr = ptr->next;
+			ptr2 = ptr->next->next;
+			count++;
+		}
+		/* Another Logic:
+		while(pos != 1){
+			temp = temp->next;
+			pos--;
+		}*/
+		if(ptr->next == NULL){
+			ptr->next = temp;
+			temp->prev = ptr;
+		} else {
+			temp->prev = ptr;
+			temp->next = ptr->next;
+			ptr->next = temp;
+			ptr2->prev = temp;
+		}
+	}
+	return head;
+}
+
+// Deleting First Node of Linked List -
+struct node* deleteFirstNode(struct node *head){
+	head = head->next;
+	free(head->prev);
+	head->prev = NULL;
+	return head;
+}
+
+// Deleting Last Node of Linked List -
+void deleteLastNode(struct node *head){
+	struct node *ptr = head;
+	while(ptr->next->next != NULL)
+		ptr = ptr->next;
+	free(ptr->next);
+	ptr->next = NULL;
+}
+
+// Deleting Node at Certain Position in Linked List -
+struct node* deleteAtPosNode(struct node *head, int pos){
+	struct node *ptr = head;
+	if(head == NULL)
+		printf("\nLinked list is already empty!");
+	else if(pos == 1)
+		head = deleteFirstNode(head);
+	else {
+		while(pos > 1){
+			ptr = ptr->next;
+			pos--;
+		}
+		if (ptr->next == NULL)
+			deleteLastNode(head);
+		else {
+			ptr->prev->next = ptr->next;
+			ptr->next->prev = ptr->prev;
+			free(ptr);
+		}
+	}
 	return head;
 }
 
@@ -68,14 +144,38 @@ int main(){
 	printData(head);
 
 	// Inserting Node at Beggining of Linked List -
-	printf("\n\n# Inserting Node at Beggining:");
+	printf("\n\n# Inserting Node at Beggining :");
 	head = insertAtBeg(head,20);
 	printData(head);
 
 	// Inserting Node at End of Linked List -
-	printf("\n\n# Inserting Node at End:");
+	printf("\n\n# Inserting Node at End :");
 	head = insertAtEnd(head,30);
 	head = insertAtEnd(head,40);
+	printData(head);
+
+	// Inserting Node at Certain Position in Linked List -
+	printf("\n\n# Inserting Node at Pos :");
+	head = insertAtPos(head,50,3);
+	printData(head);
+	head = insertAtPos(head,60,2);
+	printData(head);
+	head = insertAtPos(head,70,1);
+	printData(head);
+
+	// Deleting First Node of Linked List -
+	printf("\n\n# Deleting First Node :");
+	head = deleteFirstNode(head);
+	printData(head);
+
+	// Deleting Last Node of Linked List -
+	printf("\n\n# Deleting Last Node :");
+	deleteLastNode(head);
+	printData(head);
+
+	// Deleting Node at Certain Position in Linked List -
+	printf("\n\n# Deleting Node at Certain Position :");
+	head = deleteAtPosNode(head, 3);
 	printData(head);
 
 	return 0;
