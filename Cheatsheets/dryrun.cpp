6c7148b778912acae59d279657a79b9ef2d401fc
itspatkar <94187044+itspatkar@users.noreply.github.com>
2022-11-08 23:02:59 +0530

    Updated

--- Cheatsheets/dryrun.cpp
+++ Cheatsheets/dryrun.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 using namespace std;
 
+class MyClass {
+    public: // Public access specifier
+        int pu;
+    private: // Private access specifier
+        int pr;
+    protected: // Protected access specifier
+        int pt;
+};
+
 int main(){
+    MyClass ob;
+    ob.pu = 25; // Allowed (public)
+    ob.pr = 50; // Error: Not allowed (private)
+    ob.pt = 75; // Error: Not allowed (protected)
+
     return 0;
 }
