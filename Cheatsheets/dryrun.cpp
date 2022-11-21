c46ad31b96b45e80681ee189805c19e87e240c32
itspatkar <94187044+itspatkar@users.noreply.github.com>
2022-11-21 21:23:46 +0530

    Updated

--- Cheatsheets/dryrun.cpp
+++ Cheatsheets/dryrun.cpp
@@ -1,40 +1,11 @@
 #include <iostream>
 using namespace std;
 
-class Student {
-    private:
-        int *ptr;
-    public:
-        int roll;
-        string name;
-
-        // Constructor (parameterized) :
-        Student(int r, string n){
-            roll = r;
-            name = n;
-            ptr = new int;
-            *ptr = len;
-        }
-        // Copy Constructor :
-        Student(const Student &ob){
-            ptr = new int;
-            *ptr = *ob.ptr;
-        }
-        // Destructor :
-        ~Student(){
-            cout << "Destructor is called." << endl;
-            delete ptr;
-        }
-
-        void studentInfo(){
-            cout << "Roll : " << roll << endl;
-            cout << "Name : " << name << endl;
-        }
-};
+inline int Max(int a, int b){
+    return (a > b) ? a : b;
+}
 
 int main(){
-    Student s1(17, "Mandar");
-    s1.studentInfo();
-
+    cout << "Max of number is " << max(12,10) << endl;
     return 0;
 }
