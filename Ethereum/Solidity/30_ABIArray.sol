90c406274957ac19a13f768e7b89d7efe0168439
itspatkar <94187044+itspatkar@users.noreply.github.com>
2023-07-29 22:25:53 +0530

    Updated

--- Ethereum/Solidity/30_ABIArray.sol
+++ Ethereum/Solidity/30_ABIArray.sol
@@ -4,10 +4,14 @@ pragma solidity >=0.8.0;
 
 /* ABI Array:
 ABI stands for Application Binary Interface.
-The ABI Array contains all functions, inputs and outputs, as well as all variables and their types from a smart contract.
+The ABI Array contains all functions, inputs and outputs, returns values, as well as all variables and their types from a smart contract.
 When you compile a smart contract then you get bytecode which gets deployed.
 Inside there are assembly opcodes telling the EVM during execution of a smart contract where to jump to.
-Those jump destinations are the first 4 bytes of the keccak hash of the function signature. */
+Those jump destinations are the first 4 bytes of the keccak hash of the function signature. 
+
+- Its JSON file
+- contains all the information to interact with the contract
+*/
 
 contract MyContract {
     uint public myUint = 123;
