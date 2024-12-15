5bc41691f0ac756a8a03418b4cf33d2cf74b4acb
itspatkar <94187044+itspatkar@users.noreply.github.com>
2024-12-15 11:06:32 +0000

    Update

--- Assets/screenshot.sh
+++ Assets/screenshot.sh
@@ -1,1 +1,1 @@
-xfce4-screenshooter --clipboard --no-border --window --save /home/mandar/Pictures/"Screenshot_$(date -d "today" +"%Y-%m-%d_%H-%M-%S").png"
+xfce4-screenshooter --clipboard --no-border --window --save /home/prime/Pictures/"Screenshot_$(date -d "today" +"%Y-%m-%d_%H-%M-%S").png"
